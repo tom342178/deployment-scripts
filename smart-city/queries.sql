c11c137f605a622d81e9ab9e166ac8631194ec54
Ori Shadmon <oshadmon@gmail.com>
2024-08-29 13:55:54 -0700

    machine-builder
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- smart-city/queries.sql
+++
@@ -1,3 +0,0 @@
-{
-  "sql": "SELECT insert_timestamp, am_mx1_running, mcc_srfrng_s, am_hvefrng_s, mcc_npwprng1_s, mcc_npwprng2_s, mcc_tsnprdy_s, mcc_tstdbyrdy_s, mcc_genrng_s FROM wwp_digital ORDER BY insert_timestamp DESC limit 1"
-}
